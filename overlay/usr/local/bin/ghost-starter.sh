e1624b7124ee8c5f3ccd15864f9601c6f108749f
Rik <rikgoldman@gmail.com>
2016-03-27 18:33:44 +0000

    change to production, hopefully

--- overlay/usr/local/bin/ghost-starter.sh
+++ overlay/usr/local/bin/ghost-starter.sh
@@ -3,6 +3,6 @@
 if [ $(ps aux | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
 then
     export PATH=/usr/local/bin:$PATH
-    export NODE_ENV=development
+    export NODE_ENV=production
     NODE_ENV=development forever start --sourceDir /opt/ghost index.js >> /var/log/ghost-log 2>&1
 fi
